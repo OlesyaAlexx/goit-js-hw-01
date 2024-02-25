3313df29c2a8ac4a8fefe86e73fae816d67e4ef3
OlesyaAlexx <152548843+OlesyaAlexx@users.noreply.github.com>
2024-02-26 01:43:19 +0200

    3 tasks
    modificatin

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    quantity * pricePerDroid
+  } credits!`;
+}
+let quantity = 5;
+let pricePerDroid = 3000;
+/* const totalPrice = quantity * pricePerDroid; */
+console.log(
+  `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+);
+
+/* console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`); */
+
+makeTransaction(5, 3000); // Result 15000 ;
+makeTransaction(3, 1000); // Result 3000 ;
+makeTransaction(10, 500); // Result 5000 ;
+
+console.log(makeTransaction(5, 3000)); //"Ви замовили 5 дроїдів вартістю 15000 кредитів!" ;
+console.log(makeTransaction(3, 1000)); //"Ви замовили 3 дроїди вартістю 3000 кредитів!" ;
+console.log(makeTransaction(10, 500)); //"Ви замовили 10 дроїдів вартістю 5000 кредитів!" ;
